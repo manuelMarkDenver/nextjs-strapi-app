4f07ab6baba052d3d6e43e60fb46c13a3ce91f77
manuelMarkDenver <manuel.markdenver@gmail.com>
2023-07-26 00:42:07 +0800

    added comps on root layout~

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,8 +1,10 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Header from "./components/header";
+import Footer from './components/footer';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "NextJS Strapi App",
@@ -12,11 +14,15 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
-  )
+  );
 }
