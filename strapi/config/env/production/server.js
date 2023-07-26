bf4399b8819c233aa00928c1111ba2768327f3bf
manuelMarkDenver <manuel.markdenver@gmail.com>
2023-07-26 23:46:57 +0800

    updated server.js

--- strapi/config/env/production/server.js
+++ strapi/config/env/production/server.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
   proxy: true,
   // url: env('APP_STRAPI_URL'), // Sets the public URL of the application.
-  url: "https://j2911realty-k7b8l.ondigitalocean.app/strapi",
+  url: "http://104.248.147.209/strapi",
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
 });
