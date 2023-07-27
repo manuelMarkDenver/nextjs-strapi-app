556f9c21078e679592e2637fd4bd223e2cdae022
manuelMarkDenver <manuel.markdenver@gmail.com>
2023-07-28 03:12:22 +0800

    updated multi files

--- strapi/config/env/production/database.js
+++ strapi/config/env/production/database.js
@@ -1,17 +1,17 @@
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: "postgres",
     connection: {
-      host: env('DATABASE_HOST'),
-      port: env.int('DATABASE_PORT'),
-      database: env('DATABASE_NAME', 'strapiDB'),
-      user: env('DATABASE_USERNAME', 'mhackeedev'),
-      password: env('DATABASE_PASSWORD', 'password'),
+      host: env("DATABASE_HOST"),
+      port: env.int("DATABASE_PORT"),
+      database: env("DATABASE_NAME", "strapiDB"),
+      user: env("DATABASE_USERNAME", "postgres"),
+      password: env("DATABASE_PASSWORD", "postgres"),
       // ssl: {
       //   rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
       // },
-      ssl: false
+      ssl: false,
     },
     debug: false,
   },
