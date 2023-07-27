f4a94b6f22434750f18a168700e51c83019cfff3
manuelMarkDenver <manuel.markdenver@gmail.com>
2023-07-28 03:06:41 +0800

    updated db.js

--- strapi/config/env/production/database.js
+++ strapi/config/env/production/database.js
@@ -5,9 +5,9 @@ module.exports = ({ env }) => ({
     connection: {
       host: env('DATABASE_HOST'),
       port: env.int('DATABASE_PORT'),
-      database: env('DATABASE_NAME'),
-      user: env('DATABASE_USERNAME'),
-      password: env('DATABASE_PASSWORD'),
+      database: env('DATABASE_NAME', 'strapiDB'),
+      user: env('DATABASE_USERNAME', 'mhackeedev'),
+      password: env('DATABASE_PASSWORD', 'password'),
       // ssl: {
       //   rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
       // },
